2833c627ddb5c36a07bff75c6f6ae66f13e8b4d7
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-02-25 14:52:08 +0530

    Update Login.js

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -61,7 +61,7 @@ function Login() {
           <p style={{ color: "royalblue", fontSize: "25px" }}>
             HandCrafted with ❤️ by{" "}
           </p>
-          <h3></h3>
+          <h3>Aakash Manihar</h3>
         </div>
         <div className="login__auth">
           <div className="login__authOptions">
